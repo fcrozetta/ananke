92b6d554b42fc3742c4689fc385617efa3e0bd2b
Fernando Crozetta <fernando@czetta.com>
2024-07-24 18:08:53 +0200

    scrapy getting variable of path

--- backend/src/services/scrapy.py
+++ backend/src/services/scrapy.py
@@ -3,6 +3,7 @@ import subprocess, os, json
 
 class Spider:
     root_folder = "src/spiders/"
+    scrapy_path = os.getenv("SCRAPY_PATH", "scrapy")
 
     def _clean_file(self, filename: str):
         if os.path.exists(filename):
@@ -18,7 +19,7 @@ class Spider:
     def retrieve_data(self, spider_name: str) -> dict:
         filename = f"{spider_name}.json"
         self._clean_file(filename)
-        cmd = f"./.venv/bin/scrapy runspider src/spiders/{spider_name}_spider.py -o {filename}"
+        cmd = f"{self.scrapy_path} runspider src/spiders/{spider_name}_spider.py -o {filename}"
         process = subprocess.Popen(cmd.split())
         process.communicate()
         with open(filename, "r") as fp:
