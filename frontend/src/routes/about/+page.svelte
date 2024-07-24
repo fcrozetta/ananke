93e72b2e6816c59e4b59cf06b9bc4ce0b94dc8b4
Fernando Crozetta <fernando@czetta.com>
2024-07-24 13:52:27 +0200

    comment out useless part

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -5,7 +5,7 @@
 
 <div class="text-column">
 	<h1>About this app</h1>
-
+	<!--
 	<p>
 		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
 		following into your command line and following the prompts:
@@ -22,5 +22,5 @@
 	<p>
 		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
 		using it with JavaScript disabled!
-	</p>
+	</p> -->
 </div>
